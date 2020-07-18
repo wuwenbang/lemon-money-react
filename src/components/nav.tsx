a0c530fa5e7785984272b5aa565fce4f3ab96ec6
wuwenbang <wuwenbang@foxmail.com>
2020-07-18 09:26:30 +0800

    搞定icons

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,17 +1,31 @@
 import styled from "styled-components";
 import React from "react";
 import { Link } from "react-router-dom";
+//require 可以防止 Tree Shaking
+require("icons/money.svg");
+require("icons/tag.svg");
+require("icons/statistics.svg");
 
 const NavWrapper = styled.nav`
-  line-height: 24px;
+  line-height: 1.2;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >ul{
    display:flex;
     >li{
+      padding:4px 0;
       width: 33.33333%;
-      padding: 16px;
       text-align: center;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      flex-direction:column;
+      font-size:12px;
+      >.icon{
+          width:32px;
+          height:32px;
+      }
     }
+
   }
 `;
 
@@ -20,12 +34,21 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag"></use>
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money"></use>
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#statistics"></use>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
