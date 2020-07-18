3e3f5bb23c71a1ee46dd373e2793d2c88e237d4a
wuwenbang <wuwenbang@foxmail.com>
2020-07-18 08:55:15 +0800

    Money界面的基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
+import React from 'react';
 import styled from 'styled-components';
+import Nav from "components/nav";
+
+
 const Wrapper = styled.div`
   height:100vh;
   display:flex;
@@ -14,18 +16,9 @@ const Wrapper = styled.div`
 `;
 const Main = styled.div`
   flex-grow:1;
+  overflow:auto;
 `;
-const Nav = styled.nav`
-  >ul{
-   display:flex;
-    >li{
-      width: 33.33333%;
-      padding: 16px;
-      text-align: center;
-    }
-  }
-  border: 1px solid yellow;
-`;
+
 
 const App = function App() {
   return (
@@ -50,19 +43,8 @@ const App = function App() {
           </Switch>
         </Main>
 
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav />
+
       </Wrapper>
     </Router>
   );
